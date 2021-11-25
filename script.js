f2d11abda4c248a7009eb977225383d48a739763
ivaan1992 <ivaan.lg.92@gmail.com>
2021-11-25 16:18:47 -0600

    Install linters

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
-let library = localStorage.getItem("library") == null 
-    ? [] 
-    : JSON.parse(localStorage.getItem("library"));
-paintBooks()
+let library = localStorage.getItem('library') == null
+  ? []
+  : JSON.parse(localStorage.getItem('library'));
+paintBooks();
 
 const button = document.querySelector('#addBookButton');
 button.addEventListener('click', addBook);
 
-function book(title, author){
-    this.title = title;
-    this.author = author;
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
 }
 
 function saveLocalStorage() {
-    localStorage.setItem("library", JSON.stringify(library))
+  localStorage.setItem('library', JSON.stringify(library));
 }
 
-function addBook(e){
+function addBook(e) {
   e.preventDefault();
-  const title = document.getElementById('title').value
-  const author = document.getElementById('author').value 
-  const newBook = new book(title, author);
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const newBook = new Book(title, author);
   add(newBook);
 }
 
-function add(newBook){
+function add(newBook) {
   library.push(newBook);
   saveLocalStorage();
   paintBooks();
 }
 
 function createTR(html) {
-    const div = document.createElement("table");
-    div.innerHTML = html;
-    return div.querySelector("tr");
+  const div = document.createElement('table');
+  div.innerHTML = html;
+  return div.querySelector('tr');
 }
 
 function paintBooks() {
   const bookList = document.getElementById('book-list');
-  bookList.innerHTML = "";
-  library.forEach(book => {
+  bookList.innerHTML = '';
+  library.forEach((Book) => {
     const newBookElement = createTR(`
       <tr>
-          <td>${book.title}</td>
-          <td>${book.author}</td>
-          <td><button>X</button></td>
+          <td>${Book.title}</td>
+          <td>${Book.author}</td>
+          <td><button>Remove Book</button></td>
       </tr>
     `);
-    newBookElement.querySelector("button").addEventListener("click", () => {
-        newBookElement.remove()
-        library = library.filter(bookArr => book != bookArr)
-        saveLocalStorage();
-    })
-    bookList.appendChild(newBookElement)
+    newBookElement.querySelector('button').addEventListener('click', () => {
+      newBookElement.remove();
+      library = library.filter((bookArr) => Book !== bookArr);
+      saveLocalStorage();
+    });
+    bookList.appendChild(newBookElement);
   });
 }
