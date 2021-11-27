b3ac3f8f619e843046f3d46a79cd7d7a8b196b86
gdumani <gdumani@gmail.com>
2021-11-26 19:52:01 -0600

    Fix Book and Books classes added eventlistener

--- script-class.js
+++ script-class.js
@@ -1,44 +1,49 @@
+/* eslint-disable max-classes-per-file */
 class Book {
-  constructor(title, author) {
+  constructor() {
     this.title = document.getElementById('title').value;
     this.author = document.getElementById('author').value;
   }
 }
-
 class Books {
-  constructor() { 
+  constructor() {
     this.library = localStorage.getItem('library') == null
-  ? []
-  : JSON.parse(localStorage.getItem('library'));}
-
+      ? []
+      : JSON.parse(localStorage.getItem('library'));
+  }
 
   paintBooks() {
     const bookList = document.getElementById('book-list');
     bookList.innerHTML = '';
     this.library.forEach((book,i) => {
-      const row = document.createElement(tr);
+      const row = document.createElement('tr');
       row.innerHTML=`
             <td>${book.title}</td>
             <td>${book.author}</td>
             <td><button id= ${i} class='remove'>Remove Book</button></td>
       `;
       bookList.appendChild(row);
-    }
+    });
   }
+
+  saveLocal() { localStorage.setItem('library', JSON.stringify(this.library)); }
 
   addBook(book) {
-    const l=this.library
-    if (JSON.stringify(l[l.length-1])!=JSON.stringify(book) &&
-    book.title != '' && book.author != ''){
-      this.library = [...this.library,book]
+    const l = this.library
+    if (JSON.stringify(l[l.length - 1]) !== JSON.stringify(book)
+    && book.title !== '' && book.author !== '') {
+      this.library = [...this.library, book];
     }
-    this.paintBooks()
+    this.saveLocal();
+    this.paintBooks();
   }
 
-  removeBook(id)
-  
+  // removeBook(id);
 }
 
-
-
-
+const books = new Books();
+books.paintBooks();
+document.querySelector('#addBookButton').addEventListener('click', () => {
+  const newBook = new Book();
+  books.addBook(newBook);
+});
